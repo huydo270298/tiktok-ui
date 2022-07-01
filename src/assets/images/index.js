5cdf5d7724b7be2bd65bf0e683c50465e902ff40
DoQuangHuy <huydo@hivelab.com>
2022-07-01 14:37:13 +0700

    xay dung ui Header p1

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+  logo: require('~/assets/images/logo.svg').default,
+  icon_search: require('~/assets/images/icon_search.svg').default,
+  icon_search_reset: require('~/assets/images/icon_search_reset.svg').default,
+};
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search accounts and videos" spellCheck="false" />
+          <button className={cx('clear')}>
+            <i className={cx('icon-reset')}>
+              <img src={images.icon_search_reset} alt="" />
+            </i>
+            {/* <FontAwesomeIcon icon={faCircleXmark} /> */}
+          </button>
+          {/*  <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+          <span className={cx('bar')}></span>
+          <button className={cx('search-btn')}>
+            <i className={cx('icon-search')}>
+              <img src={images.icon_search} alt="" />
+            </i>
+          </button>
+        </div>
+        <div className={cx('acion')}></div>
+      </div>
     </header>
   );
 }
