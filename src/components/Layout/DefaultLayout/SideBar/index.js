8ad2ed4ff527897414ada3c65eacb3d87c8a828f
DoQuangHuy <huydo@hivelab.com>
2022-07-01 10:50:45 +0700

    dung khung layout mac dinh

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './SideBar.module.scss';
+
+const cx = classNames.bind(styles);
 
 function SideBar() {
-  return <h2>SideBar</h2>;
+  return (
+    <aside className={cx('wrapper')}>
+      <h2>Sidebar</h2>
+    </aside>
+  );
 }
 
 export default SideBar;
